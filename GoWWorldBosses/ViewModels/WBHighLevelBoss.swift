4bca6796e2c9c287b3f08ca75483644650d88356
Austin Chen <austin@hubub.com>
2016-08-31 18:43:34 -0400

    Changing: assets

---
+++ GoWWorldBosses/ViewModels/WBHighLevelBoss.swift
@@ -0,0 +1,13 @@
+//
+//  WBHighLevelBoss.swift
+//  GoWWorldBosses
+//
+//  Created by Austin Chen on 2016-08-29.
+//  Copyright © 2016 Austin Chen. All rights reserved.
+//
+
+import Cocoa
+
+class WBHighLevelBoss: NSObject {
+
+}
