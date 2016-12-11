c8359774e6ebd558e061558bb4cf4fbcc155757b
Austin Chen <austin@hubub.com>
2016-12-11 15:35:47 -0500

    Add: database migration schem

--- GoWWorldBosses/Application/AppDelegate.swift
+++ GoWWorldBosses/Application/AppDelegate.swift
@@ -83,6 +83,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         */
         
         
+        let config = Realm.Configuration(
+            // Set the new schema version. This must be greater than the previously used
+            // version (if you've never set a schema version before, the version is 0).
+            schemaVersion: 1,
+            
+            // Set the block which will be called automatically when opening a Realm with
+            // a schema version lower than the one set above
+            migrationBlock: { migration, oldSchemaVersion in
+                // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                if oldSchemaVersion < 1 {
+                    migration.enumerateObjects(ofType: WBCurrency.className()) { oldObject, newObject in                            newObject!["walletElement"] = nil
+                    }
+                    
+                    migration.enumerateObjects(ofType: WBBankItem.className()) { oldObject, newObject in                            newObject!["bankElement"] = nil
+                    }
+                }
+            }
+        )
+        
+        // Tell Realm to use this new configuration object for the default Realm
+        Realm.Configuration.defaultConfiguration = config
+        
         return true
     }
 
